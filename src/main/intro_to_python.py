d2ade6fa383d7f2c254b1818c258eb9a6c546a5e
Ava Johnson <avajohnson@Avas-MacBook-Air.local>
2023-01-19 14:36:34 -0500

    Output

---
+++ src/main/intro_to_python.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def printMatrix(matrix: np.ndarray):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print(int(matrix[i][j]), end =(" " if j != len(matrix[i]) - 1 else "\n"))
+    print()
+
+if __name__ == "__main__":
+    matrix = np.zeros((3,3))
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+                matrix[i][j] = 1 if (i == j) else 0
+
+    printMatrix(matrix)
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if(i != j):
+                matrix[i][j] = matrix[i][j] + 3
+
+    printMatrix(matrix)
+
+    matrix2 = np.delete(matrix, len(matrix[0]) - 1, 1)
+    
+    printMatrix(matrix2)
